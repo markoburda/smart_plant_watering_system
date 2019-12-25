8a591a17a92a29e0ae2e4378dcb5690f8a3e1f12
liteburn <arsik996@gmail.com>
2019-12-25 19:02:59 +0200

    Fixed some bugs with module

--- Wi-FiModule.ino
+++ Wi-FiModule.ino
@@ -1,48 +1,47 @@
-#include ESP8266WiFi.h
-#include WiFiClient.h
-#include ESP8266WebServer.h
+#include "ESP8266WiFi.h"
+#include "WiFiClient.h"
+#include "ESP8266WebServer.h"
  
- Replace with your network credentials
-const char ssid = NashNet;
-const char password = 0679871690;
+const char* ssid = "NashNet";
+const char* password = "0679871690";
  
-ESP8266WebServer server(80);   instantiate server at port 80 (http port)
+ESP8266WebServer server(80);  
 
 String page = "";
 int LEDPin = 13;
 void setup(void){
-  page = h1Vazonokh1pa href=LEDOnbuttonONbuttona&nbsp;a href=LEDOffbuttonOFFbuttonap;
+  page = "<h1>Simple NodeMCU Web Server</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p>";
    
   delay(1000);
   Serial.begin(115200);
-  WiFi.begin(ssid, password); begin WiFi connection
+  WiFi.begin(ssid, password);
   Serial.println();
  
-   Wait for connection
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(.);
+    Serial.print(".");
   }
   Serial.println();
-  Serial.print(Connected to );
+  Serial.print("Connected to ");
   Serial.println(ssid);
-  Serial.print(IP address );
+  Serial.print("IP address ");
   Serial.println(WiFi.localIP());
    
-  server.on(, [](){
-    server.send(200, texthtml, page);
+  server.on("/", [](){
+    server.send(200, "text/html", page);
   });
-  server.on(LEDOn, [](){
-    server.send(200, texthtml, page);
+  server.on("LEDOn", [](){
+    server.send(200, "text/html", page);
     Serial.write(1);
     delay(1000);
   });
-  server.on(LEDOff, [](){
-    server.send(200, texthtml, page);
+  server.on("LEDOff", [](){
+    server.send(200, "text/html", page);
     Serial.write(0);
   });
   server.begin();
-  Serial.println(Web server started!);
+  Serial.println("Web server started!");
 }
  
 void loop(void){
